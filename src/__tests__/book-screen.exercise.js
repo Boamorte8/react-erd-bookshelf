5e859a23f533053faee27f7dd362b922da14ea5e
Esteban Salazar <esteban.salazar@gorillalogic.com>
2022-12-28 19:07:31 -0500

    finish extra credits 5, 6 and 7 for integration testing exercise

--- src/__tests__/book-screen.exercise.js
+++ src/__tests__/book-screen.exercise.js
@@ -5,16 +5,33 @@ import {
   screen,
   waitForLoadingToFinish,
   userEvent,
+  loginAsUser,
 } from 'test/app-test-utils'
-import {buildBook} from 'test/generate'
+import {buildBook, buildListItem} from 'test/generate'
+import {server, rest} from 'test/server'
 import * as booksDB from 'test/data/books'
+import * as listItemsDB from 'test/data/list-items'
 import {App} from 'app'
 
-test('renders all the book information', async () => {
-  const book = await booksDB.create(buildBook())
-  const route = `/book/${book.id}`
+const fakeTimerUserEvent = userEvent.setup({
+  advanceTimers: () => jest.runOnlyPendingTimers(),
+})
 
-  await render(<App />, {route})
+async function renderBookScreen({user, book, listItem} = {}) {
+  user = typeof user === 'undefined' ? await loginAsUser() : user
+  book = typeof book === 'undefined' ? await booksDB.create(buildBook()) : book
+  listItem =
+    typeof listItem === 'undefined'
+      ? await listItemsDB.create(buildListItem({owner: user, book}))
+      : listItem
+  const route = `/book/${book.id}`
+  const utils = await render(<App />, {route, user})
+
+  return {...utils, user, book, listItem}
+}
+
+test('renders all the book information', async () => {
+  const {book} = await renderBookScreen({listItem: null})
 
   expect(screen.getByRole('heading', {name: book.title})).toBeInTheDocument()
   expect(screen.getByText(book.author)).toBeInTheDocument()
@@ -43,10 +60,7 @@ test('renders all the book information', async () => {
 })
 
 test('can create a list item for the book', async () => {
-  const book = await booksDB.create(buildBook())
-  const route = `/book/${book.id}`
-
-  await render(<App />, {route})
+  const {book} = await renderBookScreen({listItem: null})
 
   const addButton = screen.getByRole('button', {name: /add to list/i})
 
@@ -86,4 +100,94 @@ test('can create a list item for the book', async () => {
   expect(screen.getByRole('textbox', {name: /notes/i})).toBeInTheDocument()
   expect(screen.queryByRole('radio', {name: /star/i})).not.toBeInTheDocument()
   expect(screen.getByLabelText(/start date/i)).toBeInTheDocument()
+})
+
+test('can remove a list item for the book', async () => {
+  await renderBookScreen()
+
+  const removeButton = screen.getByRole('button', {name: /remove from list/i})
+
+  await userEvent.click(removeButton)
+  await waitForLoadingToFinish()
+
+  expect(
+    screen.queryByRole('button', {name: /remove from list/i}),
+  ).not.toBeInTheDocument()
+  expect(screen.getByRole('button', {name: /add to list/i})).toBeInTheDocument()
+})
+
+test('can mark a list item as read', async () => {
+  await renderBookScreen()
+
+  const readButton = screen.getByRole('button', {name: /mark as read/i})
+
+  await userEvent.click(readButton)
+  await waitForLoadingToFinish()
+
+  expect(
+    screen.queryByRole('button', {name: /mark as read/i}),
+  ).not.toBeInTheDocument()
+  expect(
+    screen.getByRole('button', {name: /mark as unread/i}),
+  ).toBeInTheDocument()
+  expect(screen.getAllByRole('radio', {name: /star/i})).toHaveLength(5)
+})
+
+test('can edit a note', async () => {
+  jest.useFakeTimers()
+  const newNote = 'New note text for testing purposes'
+  await renderBookScreen()
+
+  const notesInput = screen.getByRole('textbox', {name: /notes/i})
+
+  await fakeTimerUserEvent.clear(notesInput)
+  await fakeTimerUserEvent.type(notesInput, newNote)
+
+  await screen.findByLabelText(/loading/i)
+  await waitForLoadingToFinish()
+
+  expect(notesInput).toHaveValue(newNote)
+})
+
+// describe('', () => { second })
+test('shows an error message when the book fails to load', async () => {
+  await renderBookScreen({
+    book: buildBook({id: 'test_book_id'}),
+    listItem: null,
+  })
+
+  expect((await screen.findByRole('alert')).textContent).toMatchInlineSnapshot(
+    `"There was an error: Book not found"`,
+  )
+})
+
+// describe('', () => { second })
+test('note update failures are displayed', async () => {
+  const apiURL = process.env.REACT_APP_API_URL
+  jest.useFakeTimers()
+  const newNote = 'New note text for testing purposes'
+  const testErrorMessage = '__test_error_message__'
+
+  server.use(
+    rest.put(`${apiURL}/list-items/:listItemId`, async (req, res, ctx) => {
+      return res(
+        ctx.status(400),
+        ctx.json({status: 400, message: testErrorMessage}),
+      )
+    }),
+  )
+
+  await renderBookScreen()
+
+  const notesInput = screen.getByRole('textbox', {name: /notes/i})
+
+  await fakeTimerUserEvent.clear(notesInput)
+  await fakeTimerUserEvent.type(notesInput, newNote)
+
+  await screen.findByLabelText(/loading/i)
+  await waitForLoadingToFinish()
+
+  expect((await screen.findByRole('alert')).textContent).toMatchInlineSnapshot(
+    `"There was an error: ${testErrorMessage}"`,
+  )
 })
--- src/setupTests.exercise.js
+++ src/setupTests.exercise.js
@@ -1,10 +1,13 @@
 import '@testing-library/jest-dom'
+import {act, waitFor} from '@testing-library/react'
 import {queryCache} from 'react-query'
 import * as auth from 'auth-provider'
 import {server} from 'test/server'
 import * as usersDB from 'test/data/users'
 import * as listItemsDB from 'test/data/list-items'
 import * as booksDB from 'test/data/books'
+
+jest.mock('components/profiler')
 
 // enable API mocking in test runs using the same request handlers
 // as for the client-side mocking.
@@ -19,4 +22,15 @@ afterEach(async () => {
     booksDB.reset(),
     listItemsDB.reset(),
   ])
+})
+
+afterEach(async () => {
+  // waitFor is important here. If there are queries that are being fetched at
+  // the end of the test and we continue on to the next test before waiting for
+  // them to finalize, the tests can impact each other in strange ways.
+  await waitFor(() => expect(queryCache.isFetching).toBe(0))
+  if (jest.isMockFunction(setTimeout)) {
+    act(() => jest.runOnlyPendingTimers())
+    jest.useRealTimers()
+  }
 })
